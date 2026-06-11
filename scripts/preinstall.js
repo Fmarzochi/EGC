b0e0501d753079cbc0b4364f2a3f94ea3c46d939
Felipe Marzochi <fmarzochi@gmail.com>
2026-06-11 10:49:40 -0300

    fix: scope preinstall to global installs and use node for lockfile version bumps
    Signed-off-by: Felipe Marzochi <fmarzochi@gmail.com>

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -6,6 +6,8 @@ if (process.platform === 'win32') process.exit(0);
 const fs = require('fs');
 const path = require('path');
 
+if (process.env.npm_config_global !== 'true') process.exit(0);
+
 const prefix = process.env.npm_config_prefix;
 if (!prefix) process.exit(0);
 
@@ -32,7 +34,7 @@ try {
       '    # restart terminal, then:\n' +
       '    nvm install --lts && nvm use --lts\n' +
       '\n' +
-      '  or with fnm:\n' +
+      '  or with fnm (add eval "$(fnm env --use-on-cd)" to your shell profile first):\n' +
       '    brew install fnm && fnm install --lts && fnm use lts-latest\n' +
       '\n' +
       '  See: https://github.com/Fmarzochi/EGC/blob/main/TROUBLESHOOTING.md\n' +
