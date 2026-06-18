a23fd39b7098368b63e69eafca19ea8f06b59f1a
Felipe Marzochi <fmarzochi@gmail.com>
2026-06-18 20:12:47 -0300

    style: change Portuguese (Brazil) spelling in README selectors
    Signed-off-by: Felipe Marzochi <fmarzochi@gmail.com>

--- scripts/update-readme-languages.js
+++ scripts/update-readme-languages.js
@@ -5,7 +5,7 @@ const fs   = require("fs");
 const path = require("path");
 
 const LANGUAGE_NAMES = {
-  pt: "Português do Brasil",
+  pt: "Português (Brasil)",
   es: "Español",
   fr: "Français",
   de: "Deutsch",
