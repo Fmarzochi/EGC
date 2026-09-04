457cb606f7af3e7f2fdf624d09f618d841b96e9a
Felipe Marzochi <fmarzochi@gmail.com>
2026-09-04 19:27:22 -0300

    fix(hooks): mask a substitution inside a credential only where the shell would run it
    Signed-off-by: Felipe Marzochi <fmarzochi@gmail.com>

--- tests/hooks/post-bash-command-log.test.js
+++ tests/hooks/post-bash-command-log.test.js
@@ -119,6 +119,9 @@ function runTests() {
       ["curl -u $(printf u):pw https://x", 'curl -u $(<REDACTED>):<REDACTED> https://x'],
       ['curl -u "user:$(printf pw)" https://x', 'curl -u "user:<REDACTED>" https://x'],
       ["curl -u $(x `y)`):pw https://x", 'curl -u $(<REDACTED>):<REDACTED> https://x'],
+      ["curl -u 'user$(x)':pw https://x", "curl -u 'user$(x)':<REDACTED> https://x"],
+      ["curl -u user\\$(x):pw https://x", 'curl -u user\\$(x):<REDACTED> https://x'],
+
 
 
 
